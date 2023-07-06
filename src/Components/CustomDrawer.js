8bc806c35ef86109cbf591900f3e5999b28c9d9e
LUZ <balon.basket.90@gmail.com>
2023-07-05 21:58:24 -0600

    [+] Create Custom Drawer
    Create Custom Drawer with the locations

---
+++ src/Components/CustomDrawer.js
@@ -0,0 +1,10 @@
+import { DrawerContentScrollView } from "@react-navigation/drawer";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import {hp , wp} from '../Utils/ResponsiveLayout';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+ 
+//MISSING REDUX THINGS
+
+
