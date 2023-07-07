1ef08f42c2d6796def86c7fea4ed0c4e2f696c2c
LUZ <balon.basket.90@gmail.com>
2023-07-06 18:19:05 -0600

    [+] update Custom Drawer
    update Custom Drawer

--- src/Components/CustomDrawer.js
+++ src/Components/CustomDrawer.js
@@ -4,6 +4,8 @@ import { Colors } from "react-native/Libraries/NewAppScreen";
 import {hp , wp} from '../Utils/ResponsiveLayout';
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector, useDispatch } from "react-redux";
+
  
 //MISSING REDUX THINGS
 
