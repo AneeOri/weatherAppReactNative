edba73c956d7d6f6c6cb580e8ccc8a4a4f3c5962
LUZ <balon.basket.90@gmail.com>
2023-07-08 17:29:35 -0600

    [+] update App
    Adding the initial stack navigation and the provider store

--- App.js
+++ App.js
@@ -9,20 +9,55 @@ import {CustomDrawer} from './src/Components/CustomDrawer';
 import LocationScreen from './src/Screens/LocationScreen';
 import AddLocation from './src/Screens/AddLocation';
 import HomeScreen from './src/Screens/HomeScreen'; 
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
+const InictialStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen 
+         name='LocationScreen'
+         component={LocationScreen} 
+      />
+      <Stack.Screen
+         name='AddLocation'
+         component={AddLocation} 
+      />
+    </Stack.Navigator>
+  )
+}
 
+const App = () => {
 
-export default function App() {
+  const data = useSelector(state => state.weather.weatherData);
   return (
-    <View style={styles.container}>
-     <Text>kpedoooooooooo</Text>
-    </View>
+           <NavigationContainer>
+              {data.length >= 1 
+               ?
+                 <View style={styles.container}>
+                   <Text>kpedoooooooooo</Text>
+                 </View>
+               :
+                   <InictialStack/>
+              }
+            </NavigationContainer>
   );
 }
+
+export default () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persister} >
+        <App />
+      </PersistGate>
+      
+    </Provider>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
