03ec6de4d78e34da5a878b773752edefef6c14dc
LUZ <balon.basket.90@gmail.com>
2023-07-07 21:28:39 -0600

    [+] update App

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Provider, useSelector } from 'react-redux';
+import store, {persister} from './src/Redux/store';
+import {CustomDrawer} from './src/Components/CustomDrawer';
+
+import LocationScreen from './src/Screens/LocationScreen';
+import AddLocation from './src/Screens/AddLocation';
+import HomeScreen from './src/Screens/HomeScreen'; 
+
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
     </View>
   );
 }
