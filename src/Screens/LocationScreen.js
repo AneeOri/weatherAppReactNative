0a5a3d8a9c4a95d6691559aa16abc88dea2248ba
LUZ <balon.basket.90@gmail.com>
2023-07-07 21:30:11 -0600

    [+] Create Location Screen

---
+++ src/Screens/LocationScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+const LocationScreen = () => {
+    return(
+        <View>
+            <Text> Location Screen</Text>
+        </View>
+    );
+}
+
+export default LocationScreen;
