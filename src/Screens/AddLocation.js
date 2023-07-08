48d1ecab42819dd8cdfe7ed7399504b8758529fc
LUZ <balon.basket.90@gmail.com>
2023-07-07 21:29:35 -0600

    [+] Create Add Location
    Create Add Location Screen

---
+++ src/Screens/AddLocation.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+const AddLocation = () => {
+    return(
+        <View>
+            <Text> Location Screen</Text>
+        </View>
+    );
+}
+
+export default AddLocation;
