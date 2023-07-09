4d1ab184d1bfb76cf6bc87ad8b8fcbd89ebe6bf8
LUZ <balon.basket.90@gmail.com>
2023-07-09 17:46:10 -0600

    [+] update App
    Adding Expo Fonts

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Provider, useSelector } from 'react-redux';
 import store, {persister} from './src/Redux/store';
 import {CustomDrawer} from './src/Components/CustomDrawer';
+import { useFonts } from 'expo-font';
 
 import LocationScreen from './src/Screens/LocationScreen';
 import AddLocation from './src/Screens/AddLocation';
 import HomeScreen from './src/Screens/HomeScreen'; 
 import { PersistGate } from 'redux-persist/integration/react';
+import { useEffect } from 'react';
 
 
 
@@ -52,6 +54,21 @@ const HomeStack = () => {
 const App = () => {
 
   const data = useSelector(state => state.weather.weatherData);
+  const [fontsLoaded] = useFonts({
+    'Poppins-Regular' : require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold' : require('./assets/fonts/Poppins-SemiBold'),
+    'Poppins-Bold': require ('./assets/fonts/Poppins-Bold'),
+  })
+
+  useEffect (()=>{
+    if(fontsLoaded){
+
+    }
+  },[fontsLoaded])
+
+  if(!fontsLoaded){
+    return;
+  }
   return (
            <NavigationContainer>
               {data.length >= 1 
