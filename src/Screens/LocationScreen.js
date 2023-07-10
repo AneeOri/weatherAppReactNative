1f2f04f40650fdfe524d942a6bb9413ff7ef507e
LUZ <balon.basket.90@gmail.com>
2023-07-10 15:04:48 -0600

    [+] update Location Screen
    Adding styles

--- src/Screens/LocationScreen.js
+++ src/Screens/LocationScreen.js
@@ -1,4 +1,6 @@
 import { View, Text } from "react-native";
+import { FONTS } from "../Utils/Fonts";
+import { COLORS } from "../Utils/Colors";
 
 
 const LocationScreen = () => {
@@ -10,3 +12,40 @@ const LocationScreen = () => {
 }
 
 export default LocationScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: COLORS.YELLOW_COLOR,
+      justifyContent: "center",
+    },
+    contentContainer: {
+      alignItems: "center",
+      marginBottom: 20,
+    },
+    title: {
+      fontSize: wp(18),
+      color: COLORS.BLACK,
+      textAlign: "center",
+      marginHorizontal: wp(24),
+      marginVertical: hp(16),
+      fontFamily: FONTS.POPPINS_SEMIBOLD,
+    },
+    text: {
+      fontSize: wp(16),
+      fontFamily: FONTS.POPPINS_REGULAR,
+      color: COLORS.BLACK,
+    },
+    sepratorContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+      marginVertical: hp(20),
+    },
+    separator: {
+      height: StyleSheet.hairlineWidth,
+      flex: 1,
+      backgroundColor: COLORS.BLACK,
+      marginHorizontal: 12,
+    },
+  });
