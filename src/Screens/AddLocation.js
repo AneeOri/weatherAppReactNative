59a93d7690a038038986edeb1b0493c91f3b198b
LUZ <balon.basket.90@gmail.com>
2023-07-10 15:04:22 -0600

    [+] update Add Location
    Adding Styles

--- src/Screens/AddLocation.js
+++ src/Screens/AddLocation.js
@@ -1,4 +1,6 @@
 import { View, Text, SafeAreaView, StatusBar, StyleSheet,TextInput,Alert } from "react-native";
+import { FONTS } from "../Utils/Fonts";
+import { COLORS } from "../Utils/Colors";
 
 
 const AddLocation = () => {
@@ -10,3 +12,33 @@ const AddLocation = () => {
 }
 
 export default AddLocation;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: COLORS.YELLOW_COLOR,
+      justifyContent: 'center',
+    },
+    innerContainer: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+    titleText: {
+      fontSize: wp(20),
+      fontFamily: FONTS.POPPINS_SEMIBOLD,
+      color: COLORS.BLACK,
+      textAlign: 'center',
+      marginHorizontal: wp(24),
+    },
+    inputStyle: {
+      height: hp(52),
+      borderWidth: 3,
+      marginHorizontal: wp(24),
+      borderRadius: 12,
+      paddingHorizontal: wp(12),
+      marginVertical: hp(26),
+      fontSize: wp(16),
+      fontFamily: FONTS.POPPINS_REGULAR,
+      color: COLORS.BLACK
+    }
+  });
