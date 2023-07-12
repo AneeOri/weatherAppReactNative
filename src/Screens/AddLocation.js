859a1de496ea00147a674a99c418594805f6e1e2
LUZ <balon.basket.90@gmail.com>
2023-07-11 20:41:24 -0600

    [+] update Add Location

--- src/Screens/AddLocation.js
+++ src/Screens/AddLocation.js
@@ -1,11 +1,69 @@
 import { View, Text, SafeAreaView, StatusBar, StyleSheet,TextInput,Alert } from "react-native";
+import { fetchWeather } from '../Utils/ApiHelper'
+import { useNavigation } from '@react-navigation/core'
+import {useRoute} from '@react-navigation/native';
 import { FONTS } from "../Utils/Fonts";
 import { COLORS } from "../Utils/Colors";
-import { wp,hp
- } from "../Utils/ResponsiveLayout";
+import { wp,hp} from "../Utils/ResponsiveLayout";
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsLoading, setSelectedCity, setWeatherData } from "../Redux/weatherSlice";
 
+import Button from "../Components/Button";
+import Header from "../Components/Header";
+import Loader from "../Components/Loader";
+import { useEffect, useState } from "react";
 
 const AddLocation = () => {
+
+  //objects
+  const navigation = useNavigation();
+  const route = useRoute();
+  const dispatch = useDispatch();
+
+  //state variables
+  const [currentRoute, setCurrentRoute] = useState('');
+  const [city, setCity] = useState('');
+
+  //Redux Selector
+  const isLoading =  useSelector(state => state.weather.isLoading);
+  const weatherData = useSelector(state => state.weather.weatherData);
+
+  useEffect(()=>{
+    setCurrentRoute(route.params?.isFrom)
+  },[]);
+
+  // On press back button
+  const onPressBack = () => {
+    navigation.goBack();
+  }
+ 
+   // On press fetch button
+   const onPressFetch = async () => {
+    dispatch(setIsLoading(true))
+
+    let weatherResponse = await fetchWeather(city) // fetch API response
+
+      if (weatherResponse?.error) {
+        dispatch(setIsLoading(false))
+        Alert.alert('Error', weatherResponse?.error?.message)
+      } else {
+        // Updated array if more than one location
+        if (weatherData?.length >= 1) {
+          let tempArr = weatherData;
+          tempArr = [...tempArr, weatherResponse]
+          dispatch(setIsLoading(false))
+          dispatch(setWeatherData(tempArr))
+          dispatch(setSelectedCity(weatherData.length))
+        } else {
+          // push single data into array
+          dispatch(setWeatherData([weatherResponse]));
+        }
+        
+        dispatch(setIsLoading(false))
+        setCity('')
+        if (currentRoute === 'Home') navigation.goBack()
+      }
+  }
     return(
         <View>
             <Text> Location Screen</Text>
