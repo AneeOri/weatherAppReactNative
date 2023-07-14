2fb03ef8ee9bd8055213f4dafaa2d8c1668ec66d
LUZ <balon.basket.90@gmail.com>
2023-07-13 22:00:30 -0600

    [+] update Api Helper

--- src/Utils/ApiHelper.js
+++ src/Utils/ApiHelper.js
@@ -1,23 +1,23 @@
-import { Alert } from "react-native";
+import { Alert } from 'react-native';
 
 const WEATHER_API_KEY = '7177d5a9668d478184a00902231007';
 
+// URLs and EndPoint
 const weatherBaseUrl = 'https://api.weatherapi.com/v1/forecast.json?key=' + WEATHER_API_KEY;
 
 export const fetchWeather = async (city) => {
+  let data = null;
+  let url = `${weatherBaseUrl}&q=${city}&days=3&aqi=yes&alerts=yes`;
 
-    let data = null;
-    let url = `${weatherBaseUrl}&q=${city}&dt=2023-07-30&aqi=yes&alerts=yes`;
-    
-    try {
-        const response = await fetch(url, {method: 'GET'});
-        const responseJson = await response.json();
-        data = responseJson;
-    } catch (error) {
-        Alert.alert('Error' , error?.message);
-        data = null;
-    }
+  try {
+    const response = await fetch(url, { method: 'GET' });
+    const responseJson = await response.json();
+    data = responseJson;
+  } catch (error) {
+    Alert.alert('Error', error?.message);
+    data = null;
+  }
 
-    return data;
-
+  return data;
 };
+
